f3f11359a36362948033948a8b55014ee54e71d9
amanigart <amanigart@outlook.com>
2023-11-30 21:39:52 +0100

    init

---
+++ d1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine($"Puzzle #1: {SolvePuzzle1()}");
+Console.WriteLine($"Puzzle #2: {SolvePuzzle2()}");
+
+var input = System.IO.File.ReadAllLines("./input.txt").ToList();
+
+
+static string SolvePuzzle1()
+{
+    int result = default;
+
+    return result.ToString();
+}
+
+
+static string SolvePuzzle2()
+{
+    int result = default;
+
+    return result.ToString();
+}
