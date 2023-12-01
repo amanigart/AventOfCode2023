64bee4346d1303b01e2d039ab6761a97b07a031b
amanigart <amanigart@outlook.com>
2023-12-01 20:03:49 +0100

    #1

--- d1/Program.cs
+++ d1/Program.cs
@@ -1,20 +1,29 @@
 ﻿Console.WriteLine($"Puzzle #1: {SolvePuzzle1()}");
 Console.WriteLine($"Puzzle #2: {SolvePuzzle2()}");
 
-var input = System.IO.File.ReadAllLines("./input.txt").ToList();
 
-
-static string SolvePuzzle1()
+static int SolvePuzzle1()
 {
-    int result = default;
+    var input = System.IO.File.ReadAllLines("./input.txt").ToList();
+    var calibrations = new List<int>();
 
-    return result.ToString();
+    input.ForEach(str =>
+    {
+        var numStr = new String(str.Where(Char.IsDigit).ToArray());
+        if (!string.IsNullOrWhiteSpace(numStr))
+        {
+            calibrations.Add(
+                int.Parse(numStr.Length == 1 ? $"{numStr}{numStr}" : $"{numStr[0]}{numStr[^1]}"));
+        }
+    });
+
+    return calibrations.Sum();
 }
 
 
-static string SolvePuzzle2()
+static int SolvePuzzle2()
 {
     int result = default;
 
-    return result.ToString();
+    return result;
 }
