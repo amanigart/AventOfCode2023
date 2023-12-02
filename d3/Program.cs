1a30f07b97ecdcb504c685b7844f263245197084
amanigart <amanigart@outlook.com>
2023-12-02 21:57:58 +0100

    p5

---
+++ d3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine($"Puzzle #1: {SolvePuzzle1()}");
+//Console.WriteLine($"Puzzle #2: {SolvePuzzle2()}");
+
+
+static int SolvePuzzle1()
+{
+    var input = System.IO.File.ReadAllLines("./input.txt").ToList();
+    var results = new List<int>();
+
+    return results.Sum();
+}
+
+
+static int SolvePuzzle2()
+{
+    var input = System.IO.File.ReadAllLines("./input.txt").ToList();
+    var results = new List<int>();
+
+    return results.Sum();
+}
